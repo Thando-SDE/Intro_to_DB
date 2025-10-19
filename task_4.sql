6935bea68dade01ffae059cf1020d606869c8d17
Thando-SDE <ThandoMjacu@gmail.com>
2025-10-19 23:55:23 +0200

    Add task_4.sql for books table description

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- Print full description of books table using INFORMATION_SCHEMA
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
